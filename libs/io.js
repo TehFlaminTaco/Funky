01de9951c074b9632963dda33377196a4477996e
William Lemon <William.Lemon2@gmail.com>
2017-11-07 12:48:08 +1100

    Added a bunch of file stuffs
    I'll need to rename `stdin` soon...

--- libs/io.js
+++ libs/io.js
@@ -1,4 +1,6 @@
 const stdin = require("../stdin.js");
+const stdinreader = stdin(process.stdin.fd)
+const fs = require("fs");
 module.exports = function(globals){
     var objects = require("../objects.js");
     /**
@@ -27,8 +29,102 @@ module.exports = function(globals){
      * @memberof io
      * @returns {string} line - The next line of STDIN.
      */
-    io.read = function(method){
-        return stdin.read_line();
+    io.read = function(mode=""){
+        if(mode=="*n")
+            return reader.read_number();
+        else if(mode=="*a")
+            return reader.read_until("$a^")
+        else
+            return reader.read_line();
     }
+
+    /**
+     * A file to read or write to.
+     * @class File
+     * @extends Object
+     * @see io.open
+     */
+
+    /**
+     * Open a file for reading or writing.
+     * @function
+     * @memberof io
+     * @param {string} file - The path to the file relative to the CWD.
+     * @param {string} [mode="r"] - The method to open the file.
+     * @returns {File} opened - The opened file.
+     */
+    io.open = function(filename, mode="r"){
+        var fd = fs.openSync(filename, mode);
+        var file = objects.newList();
+        var f = file.vars;
+        var reader = stdin(fd);
+
+        f.descriptor = fd;
+        f.name = filename;
+        f.alive = true;
+
+        /**
+         * Read text from a file.
+         * @function
+         * @memberof File
+         * @param {File} file - The file to read from.
+         * @param {string} [mode=""] - Method to read, defaults to line by line.
+         * Methods are:
+         *  *n - Read a number
+         *  *a - Read the entire text.
+         * @returns {string} text - The read string or nil.
+         */
+        f.read = function(file, mode=""){
+            if(mode=="*n")
+                return reader.read_number();
+            else if(mode=="*a")
+                return reader.read_until("$a^")
+            else
+                return reader.read_line();
+        }
+
+        /**
+         * Write text to a file.
+         * @function
+         * @memberof File
+         * @param {File} file 
+         * @param {string} text 
+         * @returns {string} text - The input text.
+         */
+        f.write = function(file, text){
+            var buf = new Buffer(text);
+            fs.writeSync(fd, buf);
+            return text;
+        }
+
+        /**
+         * Build an iterator out of the lines in a file.
+         * Always starts from the start of the file.
+         * @function
+         * @memberof File
+         * @param {string} file - The file to iterate over
+         * @returns {function} iter - The iterator function
+         */
+        f.lines = function(file){
+            var tempReader = stdin(file.vars.descriptor);
+            return tempReader.read_line
+        }
+
+        /**
+         * Close a file, preventing it from being accessed further.
+         * @memberof File
+         * @param {File} file - The file to close.
+         * @returns {File} file - The same file, closed. Reading a closed file may have unintended behavoir.
+         */
+        f.close = function(file){
+            file.alive = false;
+            fs.closeSync(fd);
+            return file;
+        }
+
+
+        return file;
+    }
+
     return ioObj;
 }
--- stdin.js
+++ stdin.js
@@ -1,30 +1,33 @@
-// This entire ridiculous file is just to handle STDIN.
 const fs = require('fs')
 
-var BUFSIZE=256;
-var tmp = new Buffer(BUFSIZE);
-var tmp_len = 0;
-var builtStr = "";
-
-function read_until(cond){
-    var out;
-    while(!(out = builtStr.match(cond))){
-        try{
-            tmp_len = fs.readSync(process.stdin.fd, tmp, 0, BUFSIZE);
-            if(tmp_len==0){
+function newReader(fd){
+    var BUFSIZE=256;
+    var tmp = new Buffer(BUFSIZE);
+    var tmp_len = 0;
+    var builtStr = "";
+    var read_until=function(cond){
+        var out;
+        while(!(out = builtStr.match(cond))){
+            try{
+                tmp_len = fs.readSync(fd, tmp, 0, BUFSIZE);
+                if(tmp_len==0){
+                    var outp = builtStr;
+                    builtStr = "";
+                    return outp.length>0?outp:undefined;
+                }
+                builtStr += tmp.toString("utf8",0,tmp_len)
+            }catch(e){
                 return undefined;
             }
-            builtStr += tmp.toString("utf8",0,tmp_len)
-        }catch(e){
-            return undefined;
         }
+        builtStr = builtStr.substr(out[0].length);
+        return out[1];
     }
-    builtStr = builtStr.substr(out[0].length);
-    return out[1];
+    return {
+        read_until : read_until,
+        read_line : ()=>read_until(/(.*)\r?\n/),
+        read_number : ()=>read_until(/(.*)\D/)
+    }
 }
 
-module.exports = {
-    read_until : read_until,
-    read_line : ()=>read_until(/(.*)\r?\n/),
-    read_number : ()=>read_until(/(.*)\D/)
-}
+module.exports = newReader;
